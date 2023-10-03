3c8b0a6874c5528b99a5d318f9c3a845180dd8d4
ElrohirGT <elrohirgt@gmail.com>
2023-10-03 10:18:48 -0600

    feat: Basic Lab05 setup

---
+++ Laboratorio05/app/src/main/java/com/uvg/gt/lab05/HomeView.kt
@@ -0,0 +1,8 @@
+package com.uvg.gt.lab05
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeView() {
+
+}
---
+++ Laboratorio05/app/src/main/java/com/uvg/gt/lab05/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.uvg.gt.lab05
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.uvg.gt.lab05.ui.theme.Lab05Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Lab05Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainView()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainView() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "Home") {
+        composable("Home") { HomeView() }
+        composable("AnimeDetailView") { HomeView() }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab05Theme {
+        MainView()
+    }
+}
